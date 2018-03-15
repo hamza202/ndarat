791c72e09684352a25ab5b3eb3eab98ffab73be6
hamza202 <k8668975@gmail.com>
2018-03-15 13:21:09 +0200

    first

---
+++ footer.php
@@ -0,0 +1,105 @@
+<footer class="footer default">
+    <div class="footer-divider"></div>
+    <div class="container">
+        <div class="row ">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 col-lg-4 col-md-12">
+                        <div class="column-inner">
+                            <figure class="footer-logo figure">
+                                <img width="220" height="29" src="images/logo.png" class="single_image-img attachment-full" alt="img">
+                            </figure>
+                            <div class="footer-address">
+                                <p>Giambae Group LLC
+                                    <br>
+                                    Phone:
+                                    914 488 4025
+                                    <br>
+
+                                    Fax:
+                                    914 840 8575
+                                </p>
+                            </div>
+                            <figure class="figure payment-icons">
+                                <img width="342" height="21" src="images/payment.png" class="single_image-img attachment-full" alt="img">
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-2 col-md-3">
+                        <div class="column-inner custom_1484671589428">
+                            <div class="widget widget_nav_menu">
+                                <h2 class="widgettitle">STORE MENU</h2>
+                                <div class="menu-footer-store-container">
+                                    <ul id="menu-footer-store" class="menu">
+                                        <li class="menu-item"><a href="#">Best Seller</a></li>
+                                        <li class="menu-item"><a href="#">New Arrivals</a></li>
+                                        <li class="menu-item"><a href="#">Men</a></li>
+                                        <li class="menu-item"><a href="#">Women</a></li>
+                                        <li class="menu-item"><a href="#">Accessories</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-2 col-md-3">
+                        <div class="column-inner ">
+                            <div class="widget widget_nav_menu">
+                                <h2 class="widgettitle">INFORMATION?</h2>
+                                <div class="menu-footer-information-container">
+                                    <ul id="menu-footer-information" class="menu">
+                                        <li class="menu-item"><a href="#">About Us</a></li>
+                                        <li class="menu-item"><a href="#">Privacy Policy</a></li>
+                                        <li class="menu-item"><a href="#">Terms &amp; Conditions</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-2 col-md-3">
+                        <div class="column-inner ">
+                            <div class="widget widget_nav_menu">
+                                <h2 class="widgettitle">QUESTION?</h2>
+                                <div class="menu-footer-question-container">
+                                    <ul id="menu-footer-question" class="menu">
+                                        <li class="menu-item"><a href="#">Help</a></li>
+                                        <li class="menu-item"><a href="#">FQA</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-2 col-md-3">
+                        <div class="column-inner ">
+                            <div class="widget widget_nav_menu">
+                                <h2 class="widgettitle">MY ACCOUNT</h2>
+                                <div class="menu-footer-my-account-container">
+                                    <ul id="menu-footer-my-account" class="menu">
+                                        <li class="menu-item"></li>
+                                        <li class="menu-item"><a href="#">Order History</a></li>
+                                        <li class="menu-item"><a href="#">Wish List</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row footer-wrap2">
+        <div class="col-sm-12">
+            <p class="text-center lbllcopy mgb-0">Copyright © Gimbae Group– 2017.</p>
+            <div class="linda-socials widget ">
+                <div class="socials text-center">
+                    <a class="social" target="_blank" href="https://twitter.com" title="Twitter"><i class="fa fa-twitter"></i></a>
+                    <a class="social" target="_blank" href="https://facebook.com" title="Facebook"><i class="fa fa-facebook"></i></a>
+                    <a class="social" target="_blank" href="https://facebook.com" title="Google plus"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                    <a class="social" target="_blank" href="https://facebook.com" title="Behance"><i class="fa fa-behance" aria-hidden="true"></i></a>
+                    <a class="social" target="_blank" href="https://facebook.com" title="Tumblr"><i class="fa fa-tumblr" aria-hidden="true"></i></a>
+                    <a class="social" target="_blank" href="https://facebook.com" title="Instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
